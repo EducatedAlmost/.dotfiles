68ae000f888dff7a30adee42a4adcd80fcde2bd9
AlmostEducated <EducatedAlmost@gmail.com>
2021-11-01 10:23:19 +0000

    Use visual lines

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -14,6 +14,9 @@
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
+;; Configuration that needs to be loaded very early
+(setq evil-respect-visual-line-mode t)
+
 (doom! :input
        chinese
        ;;japanese
