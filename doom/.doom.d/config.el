ef78d70a278d413a48fdd813c3aee69bb39fefac
AlmostEducated <EducatedAlmost@gmail.com>
2021-11-23 19:20:46 +0000

    Decrease Emacs font size and weight

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -18,8 +18,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Fira Code" :size 24 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "CMU Serif" :size 28 :weight 'semi-light))
+(setq doom-font (font-spec :family "Fira code" :size 20 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "CMU Serif" :size 28 :weight 'light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
