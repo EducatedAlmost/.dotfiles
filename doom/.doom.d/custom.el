01102b293047852b2fa645c3ae0c7a2af6105cb1
AlmostEducated <EducatedAlmost@gmail.com>
2021-11-23 19:19:56 +0000

    Add Doom's custom.el

---
+++ doom/.doom.d/custom.el
@@ -0,0 +1,66 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(custom-safe-themes
+   '("333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "4b0e826f58b39e2ce2829fab8ca999bcdc076dec35187bf4e9a4b938cb5771dc" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "d6844d1e698d76ef048a53cefe713dbbe3af43a1362de81cdd3aefa3711eae0d" default))
+ '(eval-expression-print-level nil)
+ '(evil-digit-bound-motions '(evil-beginning-of-visual-line))
+ '(exwm-floating-border-color "#252526")
+ '(fci-rule-color "#37474F")
+ '(highlight-tail-colors ((("#232a22" "#232a21") . 0) (("#283134" "#243034") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#237AD3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#579C4C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
+ '(objed-cursor-color "#D16969")
+ '(org-link-elisp-confirm-function nil)
+ '(org-link-translation-function 'toc-org-unhrefify)
+ '(org-outline-path-complete-in-steps nil)
+ '(org-refile-use-outline-path 'file)
+ '(pdf-view-midnight-colors (cons "#d4d4d4" "#1e1e1e"))
+ '(rustic-ansi-faces
+   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(safe-local-variable-values
+   '((cider-default-cljs-repl . shadow)
+     (cider-shadow-cljs-default-options . "app")))
+ '(transient-default-level 5)
+ '(uniquify-buffer-name-style nil nil (uniquify))
+ '(vc-annotate-background "#1e1e1e")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#579C4C")
+    (cons 40 "#81a65c")
+    (cons 60 "#acb06c")
+    (cons 80 "#D7BA7D")
+    (cons 100 "#d8ab79")
+    (cons 120 "#d99c76")
+    (cons 140 "#DB8E73")
+    (cons 160 "#d38b8c")
+    (cons 180 "#cc88a6")
+    (cons 200 "#C586C0")
+    (cons 220 "#c97ca3")
+    (cons 240 "#cd7286")
+    (cons 260 "#D16969")
+    (cons 280 "#ba6c6c")
+    (cons 300 "#a37070")
+    (cons 320 "#8d7374")
+    (cons 340 "#37474F")
+    (cons 360 "#37474F")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :extend t :weight normal))))
+ '(org-level-2 ((t (:inherit outline-2 :extend t :weight normal))))
+ '(org-level-3 ((t (:inherit outline-3 :extend t :weight normal))))
+ '(org-level-4 ((t (:inherit outline-4 :extend t :weight normal))))
+ '(org-level-5 ((t (:inherit outline-5 :extend t :weight normal))))
+ '(org-level-6 ((t (:inherit outline-6 :extend t :weight normal))))
+ '(org-level-7 ((t (:inherit outline-7 :extend t :weight normal))))
+ '(org-level-8 ((t (:inherit outline-8 :extend t :weight normal))))
+ '(org-link ((t (:inherit link :foreground "#51afef" :weight normal)))))
